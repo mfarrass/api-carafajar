025e5cea24ee94e3d12c8645a06b69fc65cbd3a0
mfarrass <mfarras196@gmail.com>
2023-12-27 01:34:41 +0700

    playlist 3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Resources\PostResource;
+use App\Http\Resources\PostDetailResource;
 
 class PostController extends Controller
 {
     function index() 
     {
         $posts = Post::all();
-        return response()->json($posts);
+        return PostResource::collection($posts);
+    }
+
+    function show($id)
+    {
+        $post = Post::with('writer:id,username')->findOrFail($id);
+        return new PostDetailResource($post);
+    }
+
+    function show2($id)
+    {
+        $post = Post::findOrFail($id);
+        return new PostDetailResource($post);
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User;
 
 class Post extends Model
 {
@@ -12,4 +14,10 @@ class Post extends Model
     protected $fillable = [
         'title', 'news_content', 'author'
     ];
+
+
+    public function writer(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author', 'id');
+    }
 }
